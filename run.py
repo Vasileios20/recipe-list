d8813d8ba6018ce0a76d637f9fd904cd0e9230d4
vasileios20 <vasilis_tsim@outlook.com>
2023-07-04 15:28:34 +0100

    Add function create_recipe to get user's input for recipe's data and store them in a list

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+
+
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
@@ -18,4 +20,57 @@ data = SHEET.worksheet("recipes")
 
 recipes = data.get_all_records()
 
-print(recipes)
+ingredients = [
+    "flour",
+    "eggs",
+    "milk",
+    "salt"
+]
+
+instructions = [
+    "mix all the ingredients in a bowl",
+    "heat up a frying pan",
+    "pour mixture into the pan to cover the surface"
+]
+
+recipe1 = {
+    "name": "Crepes",
+    "serves": 2,
+    "ingredients": ingredients,
+    "instructions": instructions
+}
+
+
+
+def create_recipe():
+    """
+    Get user input for name, serves, ingredients and instructions.
+    Store these values in a list and return this list
+    """
+    recipe = []
+    print("Welcome!\n")
+    name = input("Enter the name of the recipe here:\n")
+    recipe.append(str(name))
+    print("")
+    print("Enter how many people serves.\n")
+    print("serves should be a number, ie 2\n")
+    serves = int(input("Enter serves here:\n"))
+    recipe.append(serves)
+    ingredients_list = []
+    print("")
+    print("Enter the ingredients seperated  by comma.\n")
+    print("Exapmple: eggs, flour, cream\n")
+    ingredients = input("Enter the ingredients here:\n")
+    ingredients_list.append(str(ingredients))
+    recipe.append(str(ingredients_list))
+    instructions_list = []
+    print("")
+    print("Enter the instructions seperated by comma.\n")
+    print("Exapmple: Beat the eggs in a bowl, add the cheese, cut the bacon in cubes and fry it, add the bacon to the bowl, boil the pasta, add the pasta in the bowl and stir\n")
+    instructions = input("Enter the instructions here:\n")
+    instructions_list.append(instructions)
+    recipe.append(str(instructions_list))
+    return recipe
+
+
+create_recipe()
