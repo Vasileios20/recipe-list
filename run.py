0e8c3cf7defb9d5745d9e296bb448e41430d94c3
vasileios20 <vasilis_tsim@outlook.com>
2023-07-13 13:37:12 +0100

    Update code to preyty print recipe list. Update docstrings

--- run.py
+++ run.py
@@ -86,7 +86,7 @@ def get_serves_number():
 def validate_int_input(list_item):
     """
     Inside the try check if the input is a possitive number.
-    Raises ValueError if input is not a number or if number
+    Prints message if input is not a number or if number
     is negative.
     """
     while True:
@@ -109,6 +109,7 @@ def get_ingredients_list():
     Check if ingredient is already in the list and ask user
     to insert again if there is. When ingredient has been inserted
     asks user to add another ingredient or to continue to the next function.
+    Returns the list in json encoding.
     """
     print("")
     print("Enter the ingredients, the quantity and the measurement type "
@@ -170,8 +171,8 @@ def get_measurement_type():
 def get_instructions():
     """
     Gets instructions input from the user. Prints the step number
-    for every input. Ask the user if wants to add another
-    step.
+    for every input. Ask the user if they want to add another step.
+    Stores instructions in a list.Returns the list in json encoding.
     """
 
     instructions_list = []
@@ -241,10 +242,20 @@ def create_recipe():
 
 def get_recipe_index_to_print():
     """
-    Get user option to print recipe selected. If recipe list (google sheet)
-    is empty asks user to create a new recipe. Inside the try checks if
-    user's input is a number and in range of options. Raises ValueError if
-    input is not a number or prints message if negative number or out of range.
+    Pretty print the recipe list as in the following example:
+       Name
+    -----------
+    1: recipe1
+    2: recipe2
+    ...
+
+    If recipe list (google sheet) is empty asks user to create a new recipe.
+
+    Get user option to print recipe selected.
+
+    Inside the try checks if user's input is a number and in range of options.
+    Raises ValueError if input is not a number or prints message if negative
+    number or out of range.
     """
     recipes = data.get_all_values()
     column = data.col_values(1)
@@ -252,13 +263,21 @@ def get_recipe_index_to_print():
     while len(column) <= 1:
         user_input_menu(empty_list_message)
     message = "What would you like to do next?"
+    # create a new list of string lengths using a list comprehension
+    column_lengths = [len(s) for s in column]
+    # get the largest number in the list
+    column_max_int = max(column_lengths)
+
     while True:
         try:
-            for i in column:
+            print("")
+            print("   Name")
+            print("-" * (column_max_int + 4))
+            for i in column[1:]:
                 index = column.index(i)
                 print(f"{index}: {i}")
             print("Please choose a recipe to open by "
-                  "inserting the corresponding number\n")
+                  "inserting the corresponding number.\n")
             recipe_index = int(input("Enter your option here:\n").strip())
 
             print("")
